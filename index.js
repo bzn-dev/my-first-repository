cf0a2895fd5ec5162b5d5e71849fab903ea0c318
bznfsj <rafaelbauer0406@gmail.com>
2024-02-07 16:50:38 -0300

    index.js updated

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+* Meu primeiro programa
+* Rafael Bauer
+* Aprender a sintaxe javascript
+*/
+
+/* console.log("Hello World!");
+prompt("What is your name?"); */ 
+
+const nome = "Rafael"
+var idade = 20
+
+console.log("Hello! My name is:", nome, "and I'm", idade, "years old")
